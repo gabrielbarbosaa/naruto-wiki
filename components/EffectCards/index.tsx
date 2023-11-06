a549716775fd7202fb8646d4505bd26becbe1dd1
cria-digital <dev@cria.digital>
2023-11-06 17:48:11 -0300

    render characters

--- components/EffectCards/index.tsx
+++ components/EffectCards/index.tsx
@@ -5,7 +5,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 import './styles.css';
-import { EffectCards as Effect, Autoplay } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
 import { Character } from '@/core/interfaces/AllCharacters';
 
 interface IProps {
@@ -18,12 +18,15 @@ const EffectCards = ({characters}:IProps) => {
       effect={'cards'}
       grabCursor={true}
       autoplay
-      modules={[Effect, Autoplay]}
-      className="swiper-effect"
+      slidesPerView={3}
+      spaceBetween={30}
+      loop={true}
+      modules={[Autoplay]}
+      className="h-96 swiper-effect"
     >
       {characters?.map((item, index) => (
-        <SwiperSlide className='swiper-slide-effect' key={index}>
-          <div className='h-full w-full bg-no-repeat bg-contain' style={{backgroundImage: `url('${item?.images[0]}')`}}>
+        <SwiperSlide className='swiper-slide-effect h-96 border border-red-50 w-96' key={index}>
+          <div className='h-96 w-full bg-no-repeat bg-contain' style={{backgroundImage: `url('${item?.images[0]}')`}}>
             <div>
               <p>{item.name}</p>
               <p>{item.natureType}</p>
